43cea7639b9823e2a42b0e3d3e619b0e40d51781
Henrik Petersson <44243358+hnrkcode@users.noreply.github.com>
2023-05-30 12:50:51 +0200

    Add feature to download the QR code

--- src/popup.js
+++ src/popup.js
@@ -1,5 +1,10 @@
 import qr from "qrcode";
 
+const downloadBtn = document.getElementById("download");
+const slider = document.getElementById("scale");
+const sliderValue = document.getElementById("current-scale");
+sliderValue.innerHTML = slider.value;
+
 async function getCurrentTab() {
   const queryOptions = { active: true, lastFocusedWindow: true };
   const [tab] = await chrome.tabs.query(queryOptions);
@@ -17,4 +22,30 @@ document.addEventListener("DOMContentLoaded", async () => {
     color: { dark: "#2b2a2a", light: "#ffffff" },
   };
   qr.toCanvas(canvas, currentUrl, options);
+});
+
+slider.addEventListener("input", (event) => {
+  sliderValue.innerHTML = event.target.value;
+});
+
+downloadBtn.addEventListener("click", async () => {
+  const { url } = await getCurrentTab();
+  const currentUrl = url;
+
+  const canvas = document.createElement("canvas");
+
+  const options = {
+    margin: 1,
+    scale: slider.value,
+    color: { dark: "#2b2a2a", light: "#ffffff" },
+  };
+  qr.toCanvas(canvas, currentUrl, options);
+
+  const link = document.createElement("a");
+  const image = new Image();
+
+  image.src = canvas.toDataURL("image/png");
+  link.href = image.src;
+  link.download = "image.png";
+  link.click();
 });
